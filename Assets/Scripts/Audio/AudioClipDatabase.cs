9de64299ee9e3d06d0b61c51d17f91baefa51637
Crete\Zach <zachbohn@msn.com>
2016-10-23 12:30:16 -0700

    Fixed audio bugs, added in a audio fade. Fixed title alignment. -ZB

--- Assets/Scripts/Audio/AudioClipDatabase.cs
+++ Assets/Scripts/Audio/AudioClipDatabase.cs
@@ -16,6 +16,9 @@ public class AudioClipDatabase : MonoSingleton<AudioClipDatabase>
     public AudioClip Part_7;
     public AudioClip Part_7_END;
 
+    //Variables
+    public GameObject musicAudio;
+
 	public void PlayActionChoice()
     {
 		PlaySound(ActionChoice);
@@ -33,23 +36,47 @@ public class AudioClipDatabase : MonoSingleton<AudioClipDatabase>
 
     public void PlayPart1A()
     {
-        PlaySound(Part_1A);
+        PlayMusic(Part_1A);
     }
     public void PlayPart2D()
     {
-        PlaySound(Part_2D);
+        PlayMusic(Part_2D);
     }
     public void PlayPart6()
     {
-        PlaySound(Part_6);
+        PlayMusic(Part_6);
     }
     public void PlayPart7()
     {
-        PlaySound(Part_7);
+        PlayMusic(Part_7);
     }
     public void PlayPart7END()
     {
-        PlaySound(Part_7_END);
+        PlayMusic(Part_7_END);
+    }
+
+    private void PlayMusic(AudioClip audioClip)
+    {
+        //Debug.Log("AudioClipDatabase:PlayMusic");
+
+        AudioSource audioSource;
+
+        if (musicAudio == null)
+        {
+            musicAudio = new GameObject("Music: " + audioClip.name); // create the temp object
+            musicAudio.transform.SetParent(transform);
+            audioSource = musicAudio.AddComponent<AudioSource>(); // add an audio source
+            audioSource.clip = audioClip; // define the clip
+            audioSource.spatialBlend = 0;
+            audioSource.Play(); // start the sound
+        }
+        else
+        {
+            //Debug.Log("Now Fading to new track");
+            audioSource = musicAudio.GetComponent<AudioSource>();
+            FadingAudioSource fadingAudioSource = audioSource.GetComponent<FadingAudioSource>();
+            fadingAudioSource.Fade(audioClip, 1, true);
+        }
     }
 
     private void PlaySound (AudioClip audioClip, float volume = 1)
---
+++ Assets/Scripts/Audio/FadingAudioSource.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+/// <summary>
+///   Audio source that fades between clips instead of playing them immediately.
+/// </summary>
+[RequireComponent(typeof(AudioSource))]
+public class FadingAudioSource : MonoBehaviour
+{
+    #region Fields
+
+    /// <summary>
+    ///   Volume to end the previous clip at.
+    /// </summary>
+    public float FadeOutThreshold = 0.05f;
+
+    /// <summary>
+    ///   Volume change per second when fading.
+    /// </summary>
+    public float FadeSpeed = 0.05f;
+
+    /// <summary>
+    ///   Actual audio source.
+    /// </summary>
+    private AudioSource audioSource;
+
+    /// <summary>
+    ///   Whether the audio source is currently fading, in or out.
+    /// </summary>
+    private FadeState fadeState = FadeState.None;
+
+    /// <summary>
+    ///   Next clip to fade to.
+    /// </summary>
+    private AudioClip nextClip;
+
+    /// <summary>
+    ///   Whether to loop the next clip.
+    /// </summary>
+    private bool nextClipLoop;
+
+    /// <summary>
+    ///   Target volume to fade the next clip to.
+    /// </summary>
+    private float nextClipVolume;
+
+    #endregion
+
+    #region Enums
+
+    public enum FadeState
+    {
+        None,
+
+        FadingOut,
+
+        FadingIn
+    }
+
+    #endregion
+
+    #region Public Properties
+
+    /// <summary>
+    ///   Current clip of the audio source.
+    /// </summary>
+    public AudioClip Clip
+    {
+        get
+        {
+            return audioSource.clip;
+        }
+    }
+
+    /// <summary>
+    ///   Whether the audio source is currently playing a clip.
+    /// </summary>
+    public bool IsPlaying
+    {
+        get
+        {
+            return audioSource.isPlaying;
+        }
+    }
+
+    /// <summary>
+    ///   Whether the audio source is looping the current clip.
+    /// </summary>
+    public bool Loop
+    {
+        get
+        {
+            return audioSource.loop;
+        }
+    }
+
+    /// <summary>
+    ///   Current volume of the audio source.
+    /// </summary>
+    public float Volume
+    {
+        get
+        {
+            return audioSource.volume;
+        }
+    }
+
+    #endregion
+
+    #region Public Methods and Operators
+
+    /// <summary>
+    ///   If the audio source is enabled and playing, fades out the current clip and fades in the specified one, after.
+    ///   If the audio source is enabled and not playing, fades in the specified clip immediately.
+    ///   If the audio source is not enalbed, fades in the specified clip as soon as it gets enabled.
+    /// </summary>
+    /// <param name="clip">Clip to fade in.</param>
+    /// <param name="volume">Volume to fade to.</param>
+    /// <param name="loop">Whether to loop the new clip, or not.</param>
+    public void Fade(AudioClip clip, float volume, bool loop)
+    {
+        //Debug.Log("FadingAudioSource:Fade");
+
+        if (clip == null || clip == audioSource.clip)
+        {
+            return;
+        }
+
+        this.nextClip = clip;
+        this.nextClipVolume = volume;
+        this.nextClipLoop = loop;
+
+        if (audioSource.enabled)
+        {
+            if (this.IsPlaying)
+            {
+                //Debug.Log("Should now be fading");
+                this.fadeState = FadeState.FadingOut;
+            }
+            else
+            {
+                this.FadeToNextClip();
+            }
+        }
+        else
+        {
+            this.FadeToNextClip();
+        }
+    }
+
+    /// <summary>
+    ///   Continues fading in the current audio clip.
+    /// </summary>
+    public void Play()
+    {
+        this.fadeState = FadeState.FadingIn;
+        audioSource.Play();
+    }
+
+    /// <summary>
+    ///   Stop playing the current audio clip immediately.
+    /// </summary>
+    public void Stop()
+    {
+        audioSource.Stop();
+        this.fadeState = FadeState.None;
+    }
+
+    #endregion
+
+    #region Methods
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void FadeToNextClip()
+    {
+        //Debug.Log("FadeToNextClip");
+
+        audioSource.clip = this.nextClip;
+        audioSource.loop = this.nextClipLoop;
+
+        this.fadeState = FadeState.FadingIn;
+
+        if (audioSource.enabled)
+        {
+            audioSource.Play();
+        }
+    }
+
+    private void OnDisable()
+    {
+        audioSource.enabled = false;
+        this.Stop();
+    }
+
+    private void OnEnable()
+    {
+        audioSource.enabled = true;
+        Play();
+    }
+
+    private void Update()
+    {
+        if (!audioSource.enabled)
+        {
+            return;
+        }
+
+        if (this.fadeState == FadeState.FadingOut)
+        {
+            if (audioSource.volume > this.FadeOutThreshold)
+            {
+                //Debug.Log("Fading Out!");
+                // Fade out current clip.
+                audioSource.volume -= this.FadeSpeed * Time.deltaTime;
+            }
+            else
+            {
+                // Start fading in next clip.
+                this.FadeToNextClip();
+            }
+        }
+        else if (this.fadeState == FadeState.FadingIn)
+        {
+            if (audioSource.volume < this.nextClipVolume)
+            {
+                // Fade in next clip.
+                audioSource.volume += this.FadeSpeed * Time.deltaTime;
+            }
+            else
+            {
+                // Stop fading in.
+                this.fadeState = FadeState.None;
+            }
+        }
+    }
+
+    #endregion
+}
